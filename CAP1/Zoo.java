51398acfa429df0539bd7f6bc3d6178e6f56bdc8
Maxwell H S Souza <98348163+maxwell-hgr@users.noreply.github.com>
2025-08-19 11:40:45 -0300

    4

--- CAP1/Zoo.java
+++
@@ -1,9 +0,0 @@
-
-// Escrevendo um método Main
-public class Zoo {
-	// Para executar o método main - javac Zoo.java - java Zoo argumento1 argumento2
-	public static void main(String[] args){
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-	}
-}
