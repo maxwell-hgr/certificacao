709ab0ccd0d0c789c4889c3bc89237d5cb762c28
maxwell-hgr <maxwell.hygor01@gmail.com>
2025-08-19 11:40:13 -0300

    3

---
+++ Capítulo 1/Entendendo a Estrutura de Classes/Animal.java
@@ -0,0 +1,19 @@
+
+public class Animal {
+	String nome;
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public int numeroVisitantes(int mes) {  
+		return 10;
+	}
+}
+
+class Animal2 {
+}
---
+++ Capítulo 1/Escrevendo um Método main()/Zoo.java
@@ -0,0 +1,8 @@
+
+public class Zoo {
+	// Para executar o método main - javac Zoo.java - java Zoo argumento1 argumento2
+	public static void main(String[] args){ // String[] args - String []args - String... args
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+	}
+}
