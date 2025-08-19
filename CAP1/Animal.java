39d67485c7e0b804ff96850ba68fa8ac0388586f
maxwell-hgr <maxwell.hygor01@gmail.com>
2025-08-19 11:29:48 -0300

    2

---
+++ CAP1/Animal.java
@@ -0,0 +1,20 @@
+
+// Entendendo a estrutura de Classes
+public class Animal {
+	String nome;
+
+	public String getNome(){
+		return this.nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public int numeroVisitantes(int mes) {  
+		return 10;
+	}
+}
+
+class Animal2 {
+}
